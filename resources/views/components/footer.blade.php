5b034f9e6777f9284be1bdf9caa1e09252c8df3d
Federico191 <federicoberto708@gmail.com>
2024-06-04 12:53:56 +0700

    feat : add header and footer blade

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="bg-black shadow">
+    <div class="mx-auto max-w-7xl px-4 py-2 sm:px-6 lg:px-20">
+      <h1 class="pt-2 text-3xl font-bold tracking-tight text-white text-center">ANIVERSE</h1>
+      <div class="py-1 flex flex-col gap-2 justify-center items-center">
+        <div><img src="img/baris3.svg" alt=""></div>
+        <div><img src="img/baris2.svg" alt=""></div>
+        <div><img src="img/baris1.svg" alt=""></div>
+      </div>
+      <div class="py-5 flex flex-row gap-20 justify-center items-center">
+        <div class="pt-1.5"><a href="http://instagram.com"><img src="img/socialmedia/IG.svg" alt=""></a></div>
+        <div><a href="http://youtube.com"><img src="img/socialmedia/YT.svg" alt=""></a></div>
+        <div><a href="http://twitter.com"><img src="img/socialmedia/TWT.svg" alt=""></a></div>
+        <div><a href="http://www.twitch.tv"><img src="img/socialmedia/Twitch.svg" alt=""></a></div>
+        <div><a href="http://mail.com"><img src="img/socialmedia/mail.svg" alt=""></a></div>
+      </div>
+</div>
+</footer>
