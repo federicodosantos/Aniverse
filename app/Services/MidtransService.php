992bc748606ba77adf99b4b7391be032f91b1899
Federico191 <federicoberto708@gmail.com>
2024-06-04 13:00:34 +0700

    feat : add getter function

--- app/Services/MidtransService.php
+++ app/Services/MidtransService.php
@@ -1,11 +1,21 @@
 <?php
 
+namespace App\Services;
+
+use Illuminate\Support\Facades\Log;
+use Midtrans\Config;
+
 class MidtransService {
 
     public function __construct() {
-        Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
-        Midtrans\Config::$isProduction = env('MIDTRANS_IS_PRODUCTION');
-        Midtrans\Config::$isSanitized = env('MIDTRANS_IS_SANITIZED');
-        Midtrans\Config::$is3ds = env('MIDTRANS_IS_3DS');
+        Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        Config::$isProduction = env('MIDTRANS_IS_PRODUCTION');
+        Config::$isSanitized = env('MIDTRANS_IS_SANITIZED');
+        Config::$is3ds = env('MIDTRANS_IS_3DS');
+    }
+
+    public function getServerKey()
+    {
+        return Config::$serverKey = env('MIDTRANS_SERVER_KEY');
     }
 }
