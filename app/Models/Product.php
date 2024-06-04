81beca4bce4c2812f819f4681b32a4e360fc4451
Federico191 <federicoberto708@gmail.com>
2024-06-04 13:02:11 +0700

    feat : add relationship function "user"

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
@@ -27,5 +28,10 @@ class Product extends Model
 
     public function order(): HasMany {
         return $this->hasMany(Order::class, 'product_id', 'id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 }
